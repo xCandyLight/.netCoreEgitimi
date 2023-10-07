6712de71a4e4dfc21ea59d54b29fc04b06289270
Ceren AK <ak.ceren02@outlook.com>
2023-10-07 11:19:49 +0300

    ifElseTernaryIf

---
+++ C#101 Ödevler/ifElseTernaryIf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElseTernaryIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Tünaydın !");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler!");
+            }
+
+            string sonuc = time <= 18 ? "Tünaydın!" : "İyi Geceler!";
+            sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 18 ? "Tünaydın!" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
