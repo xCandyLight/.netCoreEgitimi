0558bc5f35b71a2fd5fc71dfc106d438019587fd
Ceren AK <ak.ceren02@outlook.com>
2023-10-09 08:54:08 +0300

    whileForeach

---
+++ C#101 Ödevler/whileForeach/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            // 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program
+            //Console.WriteLine("Lütfen bir sayı giriniz: ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //int sayac = 1;
+            //int toplam = 0;
+
+            //while (sayac <= sayi)
+            //{
+            //    toplam += sayac;
+            //    sayac++;
+            //}
+            //Console.WriteLine(toplam/sayi);
+
+            // 'a' dan 'z' ye kadar tüm harfleri console a yazdır
+            //char character = 'a';
+            //while (character < 'z')
+            //{
+            //    Console.WriteLine(character);
+            //    character++;
+            //}
+
+            Console.WriteLine("------ Foreach ------");
+            string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
