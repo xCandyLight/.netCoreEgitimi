74a428719fdead44e939ffc8bcb51b97c822e5dc
Ceren AK <ak.ceren02@outlook.com>
2023-10-10 08:51:47 +0300

    metotTanimlama

---
+++ C#101 Ödevler/metotTanimlama/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotTanimlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donustiği metot_adi(parametreListesi/arguman)
+            //{
+            //    komutlar 
+            //    return;
+            //}
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+            Console.ReadKey();
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
