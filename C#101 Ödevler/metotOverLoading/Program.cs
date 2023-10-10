7a50415f2aa61ab2710300d7a123f42c02244a91
Ceren AK <ak.ceren02@outlook.com>
2023-10-10 09:05:57 +0300

    metotOverLoading

---
+++ C#101 Ödevler/metotOverLoading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotOverLoading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Out Parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int OutSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı!");
+                Console.WriteLine(OutSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4,5, out int ToplamSonucu);
+            Console.WriteLine(ToplamSonucu);
+
+            // Metot Aşırı Yükleme - Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Ceren","AK");
+
+            Console.ReadKey();
+        }
+
+        class Metotlar
+        {
+            public void Topla(int a, int b, out int toplam)
+            {
+                toplam = a + b;
+            }
+
+            public void EkranaYazdir(string veri)
+            {
+                Console.WriteLine(veri);
+            }
+
+            public void EkranaYazdir(int veri)
+            {
+                Console.WriteLine(veri);
+            }
+
+            public void EkranaYazdir(string veri1, string veri2)
+            {
+                Console.WriteLine(veri1 + veri2);
+            }
+        }
+    }
+}
