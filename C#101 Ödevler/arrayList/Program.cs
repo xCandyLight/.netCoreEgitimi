467fbaefe66e26bc35c8b0728a1debb5a82575f5
Ceren AK <ak.ceren02@outlook.com>
2023-10-10 15:05:37 +0300

    arrayList

---
+++ C#101 Ödevler/arrayList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Collections namespace
+
+            ArrayList liste = new ArrayList();
+            //liste.Add("Ceren");
+            //liste.Add(3);
+            //liste.Add(true);
+            //liste.Add('C');
+
+            //içerisindeki verilere erişim
+            //Console.WriteLine(liste[1]);
+
+            //foreach (var item in liste)
+            //    Console.WriteLine(item);
+
+            // AddRange
+            Console.WriteLine("------ Add Range ------");
+            //string[] renkler = {"kırmızı", "sarı", "yeşil"};
+            List<int> sayilar = new List<int>();
+            // liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            // Sort
+            Console.WriteLine("------ Sort ------");
+            liste.Sort();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Binary Search
+            Console.WriteLine("------ Binary Search ------");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            // Reverse
+            Console.WriteLine("------ Reverse ------");
+            liste.Reverse();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Clear
+            Console.WriteLine("------ Clear ------");
+            liste.Clear();
+
+            foreach (var item in liste)
+                Console.WriteLine(item) ;
+
+            Console.ReadKey();
+        }
+    }
+}
